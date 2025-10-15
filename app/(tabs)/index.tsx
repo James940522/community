b4423411366a34c714653626ff9545bfafb03a0d
Jaemin Jung <james940522@gmail.com>
2025-10-15 16:05:18 +0900

    Add: flexbox mission

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,43 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.parent}>
-        <View style={styles.box}>
-          <Text style={styles.text}>Hello World</Text>
-        </View>
-        <View style={styles.box2}>
-          <Text style={styles.text}>Hello World</Text>
-          <Text style={styles.text}>Hello World</Text>
-          <Text style={styles.text}>Hello World</Text>
-        </View>
+    <SafeAreaView style={styles.parent}>
+      <View style={styles.container}>
+        <Box />
+        <Box />
+        <Box />
+      </View>
+      <View style={styles.container2}>
+        <Box />
+        <Box />
+        <Box />
+      </View>
+      <View style={styles.container3}>
+        <Box />
+        <Box />
+        <Box />
       </View>
     </SafeAreaView>
   );
 }
 
+function Box() {
+  return <View style={styles.box} />;
+}
+
 const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: '100%',
+  // 여기에 스타일을 추가하고, 컴포넌트에 연결해주세요.
+  parent: {
+    justifyContent: 'space-between',
     flex: 1,
   },
-  parent: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  box: { backgroundColor: 'yellow' },
-  box2: {
-    backgroundColor: 'green',
-  },
-  text: {
-    color: 'red',
-    fontSize: 30,
-  },
-  input: {
-    fontSize: 30,
+  container: { flexDirection: 'row', justifyContent: 'flex-end', gap: 5 },
+  container2: { flexDirection: 'row', justifyContent: 'space-evenly' },
+  container3: { flexDirection: 'row', gap: 5 },
+  box: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'red',
   },
 });
