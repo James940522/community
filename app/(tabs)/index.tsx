da0c19b8dbb71d24d7de56534a2003d0455879d8
Jaemin Jung <james940522@gmail.com>
2025-10-15 15:24:39 +0900

    Add: Components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,42 @@
-import { View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
-  return <View></View>;
+  const [text, setText] = useState('');
+
+  const handleChangeText = (newText: string) => {
+    setText(newText);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.text}>Hello World</Text>
+        <TextInput
+          value={text}
+          onChangeText={handleChangeText}
+          style={styles.input}
+        />
+        <Button title="버튼이름" onPress={() => console.log('2222')} />
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flex: 1,
+  },
+  box: { backgroundColor: '' },
+  text: {
+    color: 'red',
+    fontSize: 30,
+  },
+  input: {
+    fontSize: 30,
+  },
+});
